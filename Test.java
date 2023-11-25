885201fdd21f7034b7a2c3c0863c7f46ac05b069
Kunal Behrunani <behrunanik@gmail.com>
2023-11-26 00:38:41 +0530

    java programs

--- Test.java
+++ Test.java
@@ -4,31 +4,52 @@ public class Test {
     public static void main (String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = reader.readLine();
-        String[] inputParts = input.split(" ");
+        System.out.println("Enter Cordinates...");
 
-        int x = Integer.parseInt(inputParts[0]);
-        int y = Integer.parseInt(inputParts[1]);
-        int a = Integer.parseInt(inputParts[2]);
-        int b = Integer.parseInt(inputParts[3]);
+        String rec1 = reader.readLine();
+        String rec2 = reader.readLine();
 
-        int p = Integer.min(a, b);
-        int q = Integer.max(a, b);
+        String[] rec1Parts = rec1.split(" ");
+        String[] rec2Parts = rec2.split(" ");
 
-        int ans1 = Math.abs(x-y);
-        int ans2 = Math.abs(x-p) + Math.abs(y-q);
+        int rec1x1 = Integer.parseInt(rec1Parts[0]);
+        int rec1y1 = Integer.parseInt(rec1Parts[1]);
+        int rec1x2 = Integer.parseInt(rec1Parts[2]);
+        int rec1y2 = Integer.parseInt(rec1Parts[3]);
 
-        int ans = Math.min(ans1, ans2);
+        int rec2x1 = Integer.parseInt(rec2Parts[0]);
+        int rec2y1 = Integer.parseInt(rec2Parts[1]);
+        int rec2x2 = Integer.parseInt(rec2Parts[2]);
+        int rec2y2 = Integer.parseInt(rec2Parts[3]);
+
+        int minX = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        int[] x = { rec1x1, rec1x2, rec2x1, rec2x2 };
+        int[] y = { rec1y1, rec1y2, rec2y1, rec2y2 };
+
+        for (int i = 0; i < 4; i += 1) {
+            if (minX < x[i]) {
+                minX = x[i];
+            }
+            if (maxX > x[i]) {
+                maxX = x[i];
+            }
+            if (minY < y[i]) {
+                minY = y[i];
+            }
+            if (maxY > y[i]) {
+                maxY = y[i];
+            }
+        }
+        int ans = Math.max(maxX - minX, maxY - minY);
+        System.out.println(ans);
+
+
+
         
-        System.out.println("x: " + x);
-        System.out.println("y: " + y);
-        System.out.println("a: " + a);
-        System.out.println("b: " + b);
-        System.out.println("p: " + p);
-        System.out.println("q: " + q);
-        System.out.println("ans1: " + ans1);
-        System.out.println("ans2: " + ans2);
-        System.out.println("ans: " + ans);
         
 
     }

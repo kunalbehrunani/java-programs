ebc29c4181b9533361e4e121a1737148222bf3d0
Kunal Behrunani <behrunanik@gmail.com>
2024-02-21 12:56:37 +0530

    java programs

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,14 +4,18 @@ public class HelloWorld {
 
     LinkedListV2 ll = new LinkedListV2();
     ll.insertAtEnd(10);
-    ll.insertAtEnd(24);
-    ll.insertAtEnd(36);
+    ll.insertAtEnd(20);
+    ll.insertAtEnd(30);
+    ll.insertAtIndex(1, 40);
+    ll.insertAtIndex(10, 50);
+    ll.insertAtIndex(0, 60);
     ll.print();
-    ll.insertAtIndex(1, 94);
-    ll.print();
-    ll.insertAtIndex(10, 112);
-    ll.print();
-    ll.insertAtIndex(0, 199);
-    ll.print();
+
+    LinkedListV2 ll2 = new LinkedListV2();
+    ll2.insertAtEnd(25);
+    ll2.insertAtEnd(50);
+    ll2.insertAtIndex(1, 75);
+    ll2.insertAtIndex(10, 100);
+    ll2.print();
   }
 }

91b14e23834c97d66049c163d82de99c8fcd1777
Kunal Behrunani <behrunanik@gmail.com>
2024-02-25 17:47:58 +0530

    java programs

--- Graph.java
+++ Graph.java
@@ -1,37 +1,89 @@
 import java.io.*;
 import java.util.*;
+import java.util.LinkedList;
 
 public class Graph {
   public static void main(String[] args) throws IOException {
     try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-      StringTokenizer input = new StringTokenizer(br.readLine());
-      int n = Integer.parseInt(input.nextToken());
-      int N = n + 1;
-
       /*
        * adjMat - Adjacency Matrix Representation of Graph
-       */
-      int[][] adjMat = new int[N][N];
-
-      for (int i = 1; i < N; i += 1) {
-        input = new StringTokenizer(br.readLine());
-        for (int j = 1; j < N; j += 1) {
-          adjMat[i][j] = Integer.parseInt(input.nextToken());
-        }
-      }
-
-      /*
        * adjLst - Adjacency List Representation of Graph
        */
-      ArrayList<ArrayList<Integer>> adjLst = new ArrayList<ArrayList<Integer>>();
-      adjLst = convertGraphFromAdjacencyListToAdjacencyMatrix(adjMat);
+
+      // int[][] adjMat = inputGraphInAdjacencyMatrixRepresentation(br);
+      ArrayList<ArrayList<Integer>> adjLst = inputGraphInAdjacencyListRepresentation(br);
+
+      // adjLst = convertGraphFromAdjacencyMatrixToAdjacencyList(adjMat);
+
+      System.out.print("\n----------------\n");
       printAdjacencyListGraph(adjLst);
+      System.out.print("\n----------------\n");
+      breadthFirstSearchTraversalOfGraph(adjLst, 1);
 
     }
     return;
   }
 
-  public static ArrayList<ArrayList<Integer>> convertGraphFromAdjacencyListToAdjacencyMatrix(int[][] adjMat) {
+  public static int[][] inputGraphInAdjacencyMatrixRepresentation(BufferedReader br) throws IOException {
+    System.out.print("\nEnter the size of the adjacency (square) matrix: ");
+    StringTokenizer input = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(input.nextToken());
+    int N = n + 1;
+
+    /*
+     * adjMat - Adjacency Matrix Representation of Graph
+     */
+    int[][] adjMat = new int[N][N];
+
+    System.out.print("\nEnter the Matrix: \n");
+
+    for (int i = 1; i < N; i += 1) {
+      input = new StringTokenizer(br.readLine());
+      for (int j = 1; j < N; j += 1) {
+        adjMat[i][j] = Integer.parseInt(input.nextToken());
+      }
+    }
+    return adjMat;
+  }
+
+  public static ArrayList<ArrayList<Integer>> inputGraphInAdjacencyListRepresentation(BufferedReader br)
+      throws IOException {
+    System.out.print(
+        "\nEnter Details in the Following Format:\n   First Line Contains 2 Integers - \n      n (no. of nodes)\n      m (no. of edges). \n   Then 'm' lines follow each representing an edge.\n");
+    StringTokenizer input = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(input.nextToken());
+    int m = Integer.parseInt(input.nextToken());
+
+    /*
+     * Implement in 1 based Indexing
+     */
+    n += 1;
+    m += 1;
+
+    ArrayList<ArrayList<Integer>> adjList = new ArrayList<ArrayList<Integer>>();
+
+    boolean isGraphUnidirectional = true;
+
+    for (int i = 0; i < n; i += 1) {
+      ArrayList<Integer> temp = new ArrayList<>();
+      adjList.add(temp);
+    }
+
+    for (int i = 1; i < m; i += 1) {
+      input = new StringTokenizer(br.readLine());
+      int firstNode = Integer.parseInt(input.nextToken());
+      int secondNode = Integer.parseInt(input.nextToken());
+
+      adjList.get(firstNode).add(secondNode);
+
+      if (isGraphUnidirectional) {
+        adjList.get(secondNode).add(firstNode);
+      }
+    }
+    return adjList;
+  }
+
+  public static ArrayList<ArrayList<Integer>> convertGraphFromAdjacencyMatrixToAdjacencyList(int[][] adjMat) {
     ArrayList<ArrayList<Integer>> adjLst = new ArrayList<ArrayList<Integer>>();
 
     int n = adjMat.length;
@@ -62,6 +114,39 @@ public class Graph {
       }
       System.out.print("\n");
     }
+    return;
+  }
+
+  public static void breadthFirstSearchTraversalOfGraph(ArrayList<ArrayList<Integer>> adjLst, int startingNode) {
+    int n = adjLst.size();
+    if (startingNode < 1 || startingNode > n) {
+      System.out.print("\nError: Node is not present in the Graph");
+      return;
+    }
+    Queue<Integer> queue = new LinkedList<Integer>();
+    boolean[] isNodeVisited = new boolean[n];
+    for (int i = 1; i < n; i += 1) {
+      isNodeVisited[i] = false;
+    }
+
+    queue.add(startingNode);
+    isNodeVisited[startingNode] = true;
+
+    System.out.print("\nBFS: ");
+
+    while (queue.isEmpty() == false) {
+      int currNode = queue.poll();
+      System.out.print(currNode + " | ");
+      ArrayList<Integer> neighbourNodes = adjLst.get(currNode);
+      for (int i = 0; i < neighbourNodes.size(); i += 1) {
+        int neighbourNode = neighbourNodes.get(i);
+        if (isNodeVisited[neighbourNode] == false) {
+          queue.add(neighbourNode);
+          isNodeVisited[neighbourNode] = true;
+        }
+      }
+    }
+
     return;
   }
 }
